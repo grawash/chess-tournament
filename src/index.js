321f8d804555591a71354c52a33652e9e481c28b
zurbash <duta1101@bk.ru>
2022-07-10 14:06:28 +0400

    importing and calling displayCorrectPage from displayCorrenctPage.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import './style.css';
-import build from './createLandingPage';
-import infoPage from './personalInfo';
-import {validate,populateForm} from './validateInputs'
-import championPage from './selectPage'
+import displayCorrectPage from './displayCorrectPage'
+
+displayCorrectPage()
 
 
-if(!localStorage.getItem('page') || localStorage.getItem('page')==='landing'){
-    build();
-} else if(localStorage.getItem('page')==='personal'){
-    build()
-    infoPage()
-    populateForm()
-} else {
-    build()
-    infoPage()
-    populateForm()
-    championPage()
-}
